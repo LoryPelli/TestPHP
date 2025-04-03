0b37e14f448795f4f29a5afae9827681b7b29083
lorypelli <lorypelli@proton.me>
2025-04-03 07:12:23 +0200

    fix `Header.php`

--- src/router.php
+++ src/router.php
@@ -1,8 +1,14 @@
 <?php
 require_once 'src/enums/ServerError.php';
-include_once 'src/components/Header.php';
 $file = trim(parse_url($_SERVER['REQUEST_URI'])['path'], '/') ?: 'index';
 $isAPI = str_starts_with($file, 'api');
+if (!$isAPI) {
+    $config = require_once 'src/config/index.php';
+    $page = $config[$file];
+    $title = $page['title'];
+    $description = $page['description'];
+}
+include_once 'src/components/Header.php';
 if ($isAPI && $_SERVER['REQUEST_METHOD'] != 'POST') {
     ServerError::METHOD_NOT_ALLOWED->send();
     exit(1);
@@ -12,12 +18,6 @@ $exists = file_exists($path);
 if (!$exists) {
     ServerError::NOT_FOUND->send();
     exit(1);
-}
-if (!$isAPI) {
-    $config = require_once 'src/config/index.php';
-    $page = $config[$file];
-    $title = $page['title'];
-    $description = $page['description'];
 }
 ?>
 
