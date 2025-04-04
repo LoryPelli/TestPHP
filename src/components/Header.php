e079b5056ca8b9a12cc6276584d31c65542bebb9
lorypelli <lorypelli@proton.me>
2025-04-04 22:24:04 +0200

    Open Graph!

--- src/components/Header.php
+++ src/components/Header.php
@@ -3,11 +3,13 @@
     <meta name="viewport" content="width=device-width" />
     <?php if (isset($title)): ?>
         <title>TestPHP - <?= $title ?></title>
+        <meta name="og:title" content="TestPHP - <?= $title ?>" />
     <?php else: ?>
         <title>TestPHP</title>
     <?php endif; ?>
     <?php if (isset($description)): ?>
         <meta name="description" content="<?= $description ?>" />
+        <meta name="og:description" content="<?= $description ?>" />
     <?php endif; ?>
     <script src="https://cdn.tailwindcss.com/"></script>
 </head>
