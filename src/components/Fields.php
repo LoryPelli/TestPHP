2cfeecb73c39566ee04e087d2d59e80ad4bd535b
lorypelli <lorypelli@proton.me>
2025-04-12 11:40:55 +0200

    add padding to eye icon

--- src/components/Fields.php
+++ src/components/Fields.php
@@ -3,20 +3,20 @@ require_once 'src/enums/Type.php';
 $isLogin = $type == Type::Login;
 ?>
 <span>Email:</span>
-<input name="email" autocomplete="off" type="email" required class="p-1 border-2 rounded-md" />
+<input name="email" autocomplete="off" type="email" required class="p-1 border-2 rounded-md w-60" />
 <span>Password:</span>
 <div class="flex flex-row-reverse">
-    <input name="password" autocomplete="off" type="password" required class="p-1 border-2 rounded-md" />
+    <input name="password" autocomplete="off" type="password" required class="p-1 pr-8 border-2 rounded-md w-60" />
     <button type="button" class="absolute p-1.5 cursor-pointer"></button>
 </div>
 <?php if (!$isLogin): ?>
     <span>Repeat Password:</span>
     <div class="flex flex-row-reverse">
-        <input name="repeat_password" autocomplete="off" type="password" class="p-1 border-2 rounded-md" />
+        <input name="repeat_password" autocomplete="off" type="password" class="p-1 pr-8 border-2 rounded-md w-60" />
         <button type="button" class="absolute p-1.5 cursor-pointer"></button>
     </div>
     <span>Username:</span>
-    <input name="username" autocomplete="off" required class="p-1 border-2 rounded-md" />
+    <input name="username" autocomplete="off" required class="p-1 border-2 rounded-md w-60" />
 <?php endif; ?>
 <script>
     const show = `<?= file_get_contents('svg/show.php') ?>`;
