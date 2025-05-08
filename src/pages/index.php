7a121ec04b8f09cb82b5c0cf8f4840970429738d
lorypelli <lorypelli@proton.me>
2025-05-08 21:59:59 +0200

    tell the user to login if not

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,9 +1,10 @@
+<?php $isLogged = $email && $password && !$users->check_email($email); ?>
 <nav class="flex justify-between p-2">
     <a href="/">
         <button class="p-1 border-2 rounded-md cursor-pointer">Home!</button>
     </a>
     <div class="flex items-center p-3 border-2 rounded-md gap-x-1">
-        <?php if (!$email || !$password || !$users->check_email($email)): ?>
+        <?php if (!$isLogged): ?>
             <a href="/login">
                 <button class="p-1 border-2 rounded-md cursor-pointer">Login!</button>
             </a>
@@ -23,3 +24,8 @@
         <?php endif; ?>
     </div>
 </nav>
+<?php if (!$isLogged): ?>
+    <div class="flex flex-col items-center justify-center h-screen">
+        <span class="text-6xl italic font-bold">Login to see the rest of the page!</span>
+    </div>
+<?php endif; ?>
