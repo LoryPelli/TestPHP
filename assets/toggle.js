b7ece0fa33a4d3529ea6dfdccacc18d1f47d1382
lorypelli <lorypelli@proton.me>
2025-05-25 14:37:41 +0200

    remove caps lock icon if password is visible

--- assets/toggle.js
+++ assets/toggle.js
@@ -2,7 +2,7 @@ const passwords = document.querySelectorAll('div[data-pwd]');
 const svgs = document.querySelectorAll('div[data-svg]');
 let visible = false;
 passwords.forEach((pwd, i) => {
-    if (!svgs[i].innerHTML) {
+    if (svgs[i].innerHTML.trim() == '') {
         svgs[i].innerHTML = disabledcapslock;
     }
     const input = pwd.querySelector('input');
@@ -12,11 +12,18 @@ passwords.forEach((pwd, i) => {
             : disabledcapslock;
     });
     const button = pwd.querySelector('button');
-    if (!button.innerHTML) {
+    if (button.innerHTML.trim() == '') {
         button.innerHTML = show;
     }
     button.addEventListener('click', () => {
         visible = !visible;
+        if (visible) {
+            svgs[i].style.display = 'none';
+            input.style.paddingLeft = 'calc(var(--spacing) * 1)';
+        } else {
+            svgs[i].removeAttribute('style');
+            input.removeAttribute('style');
+        }
         input.type = visible ? 'text' : 'password';
         button.innerHTML = visible ? hide : show;
     });
