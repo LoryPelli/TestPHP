83883ea4301e699f09b2091cb0aee4e95d307ec2
lorypelli <lorypelli@proton.me>
2025-07-15 09:49:54 +0200

    fix closing tags

--- src/utils/buffer.php
+++ src/utils/buffer.php
@@ -3,6 +3,7 @@ function buffer(string $buffer): string
 {
     $html = require_once 'src/minify/index.php';
     $html->doRemoveOmittedQuotes(false);
+    $html->doRemoveOmittedHtmlTags(false);
     $html->doSortCssClassNames(false);
     return $html->minify($buffer);
 }
