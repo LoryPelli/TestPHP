17387a1bc8c66c08c12078e744d83e4da89a7082
lorypelli <lorypelli@proton.me>
2025-08-27 09:08:43 +0200

    fix `Content-Type`

--- src/pages/icons.js.php
+++ src/pages/icons.js.php
@@ -1,6 +1,6 @@
 <?php
 require_once sprintf('%s/src/utils/inline.php', $root);
-header('Content-Type: application/javascript');
+header('Content-Type: text/javascript');
 ?>
 const show = '<?= inline('svg/show.php') ?>';
 const hide = '<?= inline('svg/hide.php') ?>';
