6e40b3d96286b96fb9bfa7e3f5f44d0e7838f0e9
lorypelli <lorypelli@proton.me>
2025-09-14 12:21:56 +0200

    set correct error code

--- src/errors/index.php
+++ src/errors/index.php
@@ -3,6 +3,7 @@ $root = $_SERVER['DOCUMENT_ROOT'];
 require_once sprintf('%s/vendor/autoload.php', $root);
 require_once sprintf('%s/src/utils/buffer.php', $root);
 $code = intval($_GET['code'] ?? '500');
+http_response_code($code);
 $message = match ($code) {
     404 => 'Not Found',
     405 => 'Method Not Allowed',
