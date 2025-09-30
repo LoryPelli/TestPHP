90695c8748516e34261ea9e0a196656f17056be0
lorypelli <lorypelli@proton.me>
2025-09-30 15:44:47 +0200

    `touch` doesn't override file contents

--- htpasswd.sh
+++ htpasswd.sh
@@ -5,8 +5,6 @@ FILE_PATH="/etc/nginx/.htpasswd"
 read -p "Provide Username: " username
 read -p "Provide Password: " password
 
-if [ ! -f "$FILE_PATH" ]; then
-    touch "$FILE_PATH"
-fi
+touch "$FILE_PATH"
 
 htpasswd -b "$FILE_PATH" $username $password
