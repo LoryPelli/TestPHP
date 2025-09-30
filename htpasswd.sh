e8cbd70d57b033188f4eaff79f34e9f7c251212b
lorypelli <lorypelli@proton.me>
2025-09-30 16:51:56 +0200

    improve htpasswd script

--- htpasswd.sh
+++ htpasswd.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-FILE_PATH="/etc/nginx/.htpasswd"
+handle_sigint() {
+    printf "\n"
+    exit 1
+}
 
-read -p "Provide Username: " username
-read -p "Provide Password: " password
+trap handle_sigint SIGINT
+
+echo -n "Provide Username: "
+read username
+echo -n "Provide Password: "
+read -s password
+printf "\n"
+echo -n "Provide Password: (again) "
+read -s verification_password
+printf "\n"
+
+if [[ -z "$username" || -z "$password" || -z $verification_password ]]; then
+    echo "Values cannot be empty!"
+    exit 1
+fi
+
+if [[ "$password" != "$verification_password" ]]; then
+    echo "Passwords do not match!"
+    exit 1
+fi
+
+FILE_PATH="/etc/nginx/.htpasswd"
 
 touch "$FILE_PATH"
 
-htpasswd -b "$FILE_PATH" $username $password
+htpasswd -b "$FILE_PATH" "$username" "$password"
