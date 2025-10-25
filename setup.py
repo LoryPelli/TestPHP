d16fce71270685cbc3a49007c380d5e98902e365
lorypelli <lorypelli@proton.me>
2025-10-25 10:58:31 +0200

    fix permissions

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from pathlib import Path
 src = Path('.env.example')
 dest = Path('.env')
 htpasswd = Path('.htpasswd')
+htpasswdsh = Path('htpasswd.sh')
 
 if not(dest.exists()):
     dest.write_text(src.read_text())
 
 htpasswd.touch()
+
+htpasswdsh.chmod(0o755)
