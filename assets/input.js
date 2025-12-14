97462ae18e03e389b0295874cfe21d125eb76d9d
lorypelli <lorypelli@proton.me>
2025-12-14 20:34:28 +0100

    fixes on `input.js` file

--- assets/input.js
+++ assets/input.js
@@ -1,41 +1,32 @@
 const inputs = document.querySelectorAll('input');
 inputs.forEach((input, i) => {
-    if (i != 0 && input.value.length == 0) {
-        input.disabled = true;
-    }
     input.addEventListener('input', () => {
         input.value = (input.value[0] || '').replace(/[^0-9]/, '');
         if (input.value.length == 1) {
-            (inputs[i + 1] || {}).disabled = false;
             inputs[i + 1]?.focus();
         }
     });
     input.addEventListener('keydown', (e) => {
-        if (i != 0 && e.key == 'Backspace' && input.value.length == 0) {
-            input.disabled = true;
-            (inputs[i - 1] || {}).disabled = false;
-            inputs[i - 1]?.focus();
-        } else if (e.key == 'ArrowLeft') {
-            if (input.value.length == 0) {
-                input.disabled = true;
-            }
-            (inputs[i - 1] || {}).disabled = false;
+        if (
+            e.key == 'ArrowLeft' ||
+            (e.key == 'Backspace' && input.value.length == 0)
+        ) {
+            e.preventDefault();
             inputs[i - 1]?.focus();
         } else if (e.key == 'ArrowRight') {
-            (inputs[i + 1] || {}).disabled = false;
+            e.preventDefault();
             inputs[i + 1]?.focus();
         }
     });
     input.addEventListener('paste', (e) => {
         e.preventDefault();
-        const data = (e.clipboardData?.getData('text') || '').replaceAll(
-            /[^0-9]/,
+        const data = (e.clipboardData?.getData('text') || '').replace(
+            /[^0-9]/g,
             '',
         );
         const chars = data.trim().split('');
         for (let j = 0; j < chars.length; j++) {
             if (i + j < inputs.length) {
-                inputs[i + j].disabled = false;
                 inputs[i + j].value = chars[j];
             }
         }
